71df9795271c7d321fceb55fcb7bb54a8556c101
fachryfer <fachrysembiring424@gmail.com>
2024-12-18 13:03:05 +0700

    z

--- resources/views/layouts/frontbase.blade.php
+++ resources/views/layouts/frontbase.blade.php
@@ -3,33 +3,35 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="description" content="@yield("description")">
-    <meta name="keywords" content="@yield("keywords")">
-    <meta name="viewport" content="@yield("title")">
+    <meta name="description" content="@yield('description')">
+    <meta name="keywords" content="@yield('keywords')">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="author" content="Fachry Ferdiansyah Sembiring">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>@yield("title")</title>
-    <link rel="icon" type="image/x-icon" href="@yield("icon")">
+    <title>@yield('title')</title>
+    <link rel="icon" type="image/x-icon" href="@yield('icon')">
+
     <!-- Google Font -->
     <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;400;600;900&display=swap" rel="stylesheet">
 
     <!-- Css Styles -->
-    <link rel="stylesheet" href="{{asset('assets')}}/css/bootstrap.min.css" type="text/css">
-    <link rel="stylesheet" href="{{asset('assets')}}/css/font-awesome.min.css" type="text/css">
-    <link rel="stylesheet" href="{{asset('assets')}}/css/elegant-icons.css" type="text/css">
-    <link rel="stylesheet" href="{{asset('assets')}}/css/nice-select.css" type="text/css">
-    <link rel="stylesheet" href="{{asset('assets')}}/css/jquery-ui.min.css" type="text/css">
-    <link rel="stylesheet" href="{{asset('assets')}}/css/owl.carousel.min.css" type="text/css">
-    <link rel="stylesheet" href="{{asset('assets')}}/css/slicknav.min.css" type="text/css">
-    <link rel="stylesheet" href="{{asset('assets')}}/css/style.css" type="text/css">
-    @yield("head")
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}" type="text/css">
+    <link rel="stylesheet" href="{{ asset('assets/css/font-awesome.min.css') }}" type="text/css">
+    <link rel="stylesheet" href="{{ asset('assets/css/elegant-icons.css') }}" type="text/css">
+    <link rel="stylesheet" href="{{ asset('assets/css/nice-select.css') }}" type="text/css">
+    <link rel="stylesheet" href="{{ asset('assets/css/jquery-ui.min.css') }}" type="text/css">
+    <link rel="stylesheet" href="{{ asset('assets/css/owl.carousel.min.css') }}" type="text/css">
+    <link rel="stylesheet" href="{{ asset('assets/css/slicknav.min.css') }}" type="text/css">
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}" type="text/css">
+
+    @yield('head')
 </head>
 
 <body>
-@include("home.header")
+    @include('home.header')
 
-@yield('content')
+    @yield('content')
 
-@include("home.footer")
+    @include('home.footer')
 </body>
 </html>
